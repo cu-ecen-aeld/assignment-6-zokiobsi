08a93e91113a9afae4aca2cb09bcb9c95940b7b4
Joshua Roberson <joshua.roberson@colorado.edu>
2026-07-11 19:14:00 -0600

    Implemented changes for assignment 6 part 2

--- build.sh
+++ build.sh
@@ -2,19 +2,22 @@
 # Script to build image for qemu.
 # Author: Siddhant Jajoo.
 
+#init and update poky sudmodule 
+#submodule needs to be commited to be "detected"
 git submodule init
 git submodule sync
 git submodule update
 
 # local.conf won't exist until this step on first execution
+#this CDs into build directory for build 
 source poky/oe-init-build-env
 
 CONFLINE="MACHINE = \"qemuarm64\""
 
-cat conf/local.conf | grep "${CONFLINE}" > /dev/null
+cat conf/local.conf | grep "${CONFLINE}" > /dev/null # check local.conf is configured correctly 
 local_conf_info=$?
 
-if [ $local_conf_info -ne 0 ];then
+if [ $local_conf_info -ne 0 ];then # sets up local.conf for you for build target
 	echo "Append ${CONFLINE} in the local.conf file"
 	echo ${CONFLINE} >> conf/local.conf
 	
@@ -23,15 +26,15 @@ else
 fi
 
 
-bitbake-layers show-layers | grep "meta-aesd" > /dev/null
+bitbake-layers show-layers | grep "meta-aesd" > /dev/null #check build layer 
 layer_info=$?
 
 if [ $layer_info -ne 0 ];then
-	echo "Adding meta-aesd layer"
+	echo "Adding meta-aesd layer" # add build layer for you 
 	bitbake-layers add-layer ../meta-aesd
 else
 	echo "meta-aesd layer already exists"
 fi
 
 set -e
-bitbake core-image-aesd
+bitbake core-image-aesd #start the image build 
